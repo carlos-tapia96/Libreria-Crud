5e83ffc215924d09c1fa98a9280212af4cc98193
carlos tapia <johnncarlos7@gmail.com>
2023-09-18 18:41:03 -0400

    ADD crear, eliminar, editar y vistas

--- libreria/views.py
+++ libreria/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
+from .forms import LibroForm
 
 # Create your views here.
 def inicio(request):
@@ -16,16 +17,25 @@ def libros(request):
 
 
 
-
-
 def crear(request):
-    return render (request, 'libros/crear-libro.html')
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect ('libros')
+    return render (request, 'libros/crear-libro.html', {'formulario': formulario})
 
-def editar(request):
-    return render (request, 'libros/editar-libro.html')
+def editar(request, id):
+    libro = Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros')
+    return render (request, 'libros/editar-libro.html', {'formulario':formulario})
 
-
-
+def eliminar (request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')
 
 
 
